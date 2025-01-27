c67dda7b81e875d1a5acfa9a96062dba7abb14f3
Vladimir <sergeevich.belusha@gmail.com>
2025-01-27 19:51:26 +0300

    fix

--- src/components/Data/Data.tsx
+++ src/components/Data/Data.tsx
@@ -47,27 +47,25 @@ export default function Data({ category }: DataProps) {
 
   return (
     <>
-      {sites.length > 0 ? (
-        sites.map((site, index) => (
-          <div key={index} className="site--card">
-            <a href={site.link} target="_blank" rel="noopener noreferrer">
-              <div className="site-item">
-                <Image
-                  src={site.image}
-                  alt={site.title}
-                  width={600}
-                  height={400}
-                  loading="lazy"
-                  quality={80}
-                />
-                <h6>{site.title}</h6>
-              </div>
-            </a>
-          </div>
-        ))
-      ) : (
-        <div>Нет данных для категории '{category}'.</div>
-      )}
+      {sites.length > 0
+        ? sites.map((site, index) => (
+            <div key={index} className="site--card">
+              <a href={site.link} target="_blank" rel="noopener noreferrer">
+                <div className="site-item">
+                  <Image
+                    src={site.image}
+                    alt={site.title}
+                    width={600}
+                    height={400}
+                    loading="lazy"
+                    quality={80}
+                  />
+                  <h6>{site.title}</h6>
+                </div>
+              </a>
+            </div>
+          ))
+        : null}
     </>
   );
 }
